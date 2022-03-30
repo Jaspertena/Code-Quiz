1e0c77feed76cd9a194b203632f81717c0659aa5
Maddox Twin1 <jaspertena@aol.com>
2022-03-30 00:01:23 -0400

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # Code-Quiz
+
+<p>THis is an attempt at at Code quiz. There are many elements here but I was unable to provide the full detail as outlined in the homework read me.  This
+
+
+
+
--- script.js
+++ script.js
@@ -13,7 +13,7 @@ function beginGame() {
     // shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
     questionContainerElement.classList.remove('hide')
-    NextQuestion()
+    showQuestion()
 }
 
 function NextQuestion() {
@@ -25,85 +25,58 @@ function showQuestion(question){
     // questions[question].question
     // questionElement.textContent = question.question
     console.log(questions[0].question)
-    question.answer.foreach(answer => {
+    questions.answers.forEach(answer => {
         var button = docment.createElement('button')
         button.innerText - answer.text
         button.classList.add('btn')
-        if (answer.correct)   
-            button.dataset.correct - answer.correct
-    button.addEventListener('click'),
-    answerButtonsElementappendlChild(button)
+        if (answer.correct)  {
+            button.dataset.correct - answer.correct}
+    button.addEventListener('click',selectAnswer)
+    answerButtonsElement.appendChild(button)
     })
 }
 
+
 function selectAnswer() {
-     
 }
 
 var questions = [
-    {   question: "CDN stand for?",
-        answers: [
+    {   
+            question: "CDN stand for?",
+            answers: [
             {text: "Christian Dating Network"},
             {text: "Content Delivery Network"},
             {text: "Content Data Network"},
             {text: "Console Delivery Network"},
         ]
-    }
-      
-    {   question: "CSS stands for?",
-        answers: [
+    },
+
+    {
+            question: "CSS stands for?",
+            answers: [
             {text: "Cascading Style Sheets"},
             {text: "Content Style Sheets"},
-            {text: "Cascading Data Network"},
-            {text: "Console Delivery Network"},
+            {text: "Cascading Series Systems"},
+            {text: "Cookies Style Service"},
         ]
-    }
+    },
+
+    {  
+            question: "Which tag below is not a semantic tag?",
+            answers: [
+            {text: "<section>"},
+            {text: "<header>"},
+            {text: "<nav>"},
+            {text: "<span>"},
+        ]
+    },
+
+    {   question: "key:value paring is called?",
+        answers: [
+        {text: "function"},
+        {text: "element"},
+        {text: "property"},
+        {text: "div"},
+    ]
+}
 ]
-
-// var Startcontainer = document.querySelector(".Startcontainer");
-
-// var questionContainer = document.querySelector(".questionContainer");
-// var answerList = document.querySelector(".answerList");
-// var nextButton = document.querySelector(".nextButton");
-// var endContainer = document.querySelector(".endContainer");
-// var questionDisplay = document.querySelector(".question");
-
-// var questions = [
-//     {
-//          Question: "What does CDN stand for?",
-//         choices: ["Christian Dating Network", "Content Delivery Network", "Content Data Network", "Console Delivery Network"],
-//         correctAnswer: "2"
-//     },
-//     {
-//         Question: "1+2",
-//         choices: ["1", "2", "3", "4"],
-//         correctAnswer: "3"
-//     },
-//     {
-//         Question: "1+3",
-//         choices: ["1", "2", "3", "4"],
-//         correctAnswer: "4"
-//     },
-//     {
-//         Question: "",
-//         choices: ["1", "2", "3", "4"],
-//         correctAnswer: "1"
-//     },
-// ];
-// function showQuestion() {
-//     for (var i = 0; i < questions.length; i++){
-//     //     console.log(questions[i].Question)
-//     //     console.log(questions[i].choices)
-//     //     console.log(questions[i].correctAnswer)
-//     var questionText = questions[i].Question;
-//     questionDisplay.textContent=questionText;
-//     var answerChoices = questions[i].choices;
-//     }
-//     answerChoices.forEach(function(item){
-//         var answerbutton = document.createElement("button");
-//         answerbutton.textContent=item;
-//         answerList.appendChild(answerbutton);
-//     })
-// }
-
-// showQuestion()
