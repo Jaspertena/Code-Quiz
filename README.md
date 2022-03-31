7816c8cd6811e10fdf6bcfb9612f376891bfda22
Maddox Twin1 <jaspertena@aol.com>
2022-03-30 22:22:32 -0400

    update readme

--- README.md
+++ README.md
@@ -1,7 +1,17 @@
 # Code-Quiz
 
-<p>THis is an attempt at at Code quiz. There are many elements here but I was unable to provide the full detail as outlined in the homework read me.  This
+<p>THis is an attempt at at Code quiz. There are many elements here but I was unable to provide the full detail as outlined in the homework read me requirement.  
 
+Cer
+
+
+<https://jaspertena.github.io/Code-Quiz/>
+
+Acknowledgment
+Web Dev Simplified - youTube Video - <https://us02web.zoom.us/j/82322465062?pwd=QTNDMTI5R2djNFZNTnRRTmY2WWp3Zz09>
+
+Derrick Morton
+James Edwards 
 
 
 
--- script.js
+++ script.js
@@ -2,8 +2,10 @@ var startButton = document.getElementById('start-btn')
 var questionContainerElement = document.getElementById('question-container')
 var questionElement = document.getElementById('question')
 var answerButtonsElement = document.getElementById('answer-buttons')
+var nextButton = document.getElementById('next-btn')
 
 // var shuffledQuestions = currentQuestionIndex
+let score = 0;
 
 startButton.addEventListener('click', beginGame)
 
@@ -13,20 +15,30 @@ function beginGame() {
     // shuffledQuestions = questions.sort(() => Math.random() - .5)
     currentQuestionIndex = 0
     questionContainerElement.classList.remove('hide')
-    showQuestion()
+    showQuestion(questions[0].question)
 }
 
-function NextQuestion() {
-    
+function NextQuestion(e) {
+    console.log("clicked", e)
 }
+
+
 NextQuestion();
+nextButton.addEventListener('click', NextQuestion())
+
+
 
 function showQuestion(question){
+    
+
     // questions[question].question
     // questionElement.textContent = question.question
     console.log(questions[0].question)
+    let paragraph = document.getElementById("question");
+    let text = document.createTextNode(question)
+    paragraph.appendChild(text);
     questions.answers.forEach(answer => {
-        var button = docment.createElement('button')
+    var button = docment.createElement('button')
         button.innerText - answer.text
         button.classList.add('btn')
         if (answer.correct)  {
@@ -42,7 +54,7 @@ function selectAnswer() {
 
 var questions = [
     {   
-            question: "CDN stand for?",
+            question: "CDN stand for _________?",
             answers: [
             {text: "Christian Dating Network"},
             {text: "Content Delivery Network"},
